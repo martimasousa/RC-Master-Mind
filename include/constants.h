b5a95da16f1e6f0257352033f615df97cf94f9fa
Martim Sousa <martim.aires.de.sousa@tecnico.ulisboa.pt>
2024-11-17 20:44:23 +0000

    Client start command sending message to server

--- include/constants.h
+++ include/constants.h
@@ -5,7 +5,7 @@
 #define TRUE                1
 
 #define localhost           "127.0.0.1"
-#define GSPORT              58000
+#define GSPORT              "58000"
 #define GROUP_NUMBER        0               // TODO: Put the right value once we know our GROUP_NUMBER
 
 #define MAX_PLAYER_COMMAND  100             // TODO: Verify
